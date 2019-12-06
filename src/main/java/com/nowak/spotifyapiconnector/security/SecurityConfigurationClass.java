980a51433f8b342e8c0ada47d95f9a680bbe6a30
Kamila Nowak <knowak242@gmaik.com>
2019-12-06 23:13:25 +0100

    fetch users data upgrade

--- src/main/java/com/nowak/spotifyapiconnector/security/SecurityConfigurationClass.java
+++ src/main/java/com/nowak/spotifyapiconnector/security/SecurityConfigurationClass.java
@@ -1,10 +1,10 @@
 package com.nowak.spotifyapiconnector.security;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableOAuth2Sso
@@ -12,6 +12,7 @@ public class SecurityConfigurationClass extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/user").authenticated();
+        http.authorizeRequests().antMatchers("/users/**").authenticated()
+                                .antMatchers("/data/**").authenticated();
     }
 }
--- src/main/java/com/nowak/spotifyapiconnector/users/UserData.java
+++ src/main/java/com/nowak/spotifyapiconnector/users/UserData.java
@@ -1,16 +1,42 @@
 package com.nowak.spotifyapiconnector.users;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.security.Principal;
 
 @RestController
 public class UserData {
 
-    @GetMapping("/user")
-    public Principal getUserData(Principal principal) {
+    @GetMapping("/users/{user_id}")
+    public String getUserData(OAuth2Authentication details,@PathVariable("user_id") String id, Principal principal) {
+
+        String token = ((OAuth2AuthenticationDetails) details.getDetails()).getTokenValue();
+        System.out.println(token);
+        System.out.println(id);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer" + token);
+        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+
+        ResponseEntity<String> result =restTemplate.exchange("https://api.spotify.com/v1/users/"+id, HttpMethod.GET,httpEntity,String.class);
+        return result.getBody();
+    }
+
+    @GetMapping("/data")
+    public Principal getUserDdataata(Principal principal,OAuth2Authentication details) {
+        String token = ((OAuth2AuthenticationDetails) details.getDetails()).getTokenValue();
+        System.out.println(token);
+
         return principal;
     }
 
