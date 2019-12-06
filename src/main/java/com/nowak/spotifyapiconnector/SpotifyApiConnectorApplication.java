5f2b2c3ae30e9227e38b8d75d8c29c9bc2847a8c
Kamila Nowak <knowak242@gmaik.com>
2019-12-06 12:41:58 +0100

    Initial commit

---
+++ src/main/java/com/nowak/spotifyapiconnector/SpotifyApiConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.nowak.spotifyapiconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyApiConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyApiConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nowak/spotifyapiconnector/SpotifyApiConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowak.spotifyapiconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApiConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
