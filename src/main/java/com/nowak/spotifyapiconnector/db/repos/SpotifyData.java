43da22cb76bf7ada38539463893dbf170817562f
Kamila Nowak <knowak242@gmaik.com>
2019-12-09 23:19:08 +0100

    Added POJOs

---
+++ src/main/java/com/nowak/spotifyapiconnector/db/repos/SpotifyData.java
@@ -0,0 +1,9 @@
+//package com.nowak.spotifyapiconnector.db.repos;
+//
+//import com.nowak.spotifyapiconnector.db.entities.SpotifyUsersData;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface SpotifyData extends JpaRepository<SpotifyUsersData,Integer> {
+//}
